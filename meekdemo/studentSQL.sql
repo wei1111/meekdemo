0cfc3e4b3a796262764a5b50b70e7e0358583c35
wei1111 <1204429556@qq.com>
2018-05-09 23:35:09 +0800

    提交SQL

---
+++ meekdemo/studentSQL.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.21 : Database - mybatisdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mybatisdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `mybatisdb`;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `stuId` varchar(20) NOT NULL COMMENT '学号',
+  `password` varchar(30) NOT NULL COMMENT '密码',
+  `name` varchar(20) DEFAULT NULL COMMENT '姓名',
+  `age` int(5) DEFAULT NULL COMMENT '年龄',
+  `gender` varchar(5) DEFAULT NULL COMMENT '性别',
+  `photoPath` varchar(200) DEFAULT NULL COMMENT '头像路径',
+  `access` varchar(20) NOT NULL COMMENT '权限',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `stuId` (`stuId`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
